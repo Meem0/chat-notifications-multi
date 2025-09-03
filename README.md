c570452e26c9ceffa8e48dd86568171112f027a8
Meem0 <themeem0@gmail.com>
2025-09-02 17:12:31 -0700

    Basic copy of Chat Notifications plugin

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example
-An example greeter plugin
+# ChatNotificationsMulti
+An chatnotificationsmulti greeter plugin
--- src/test/java/com/chatnotificationsmulti/ChatNotificationsMultiPluginTest.java
+++ src/test/java/com/chatnotificationsmulti/ChatNotificationsMultiPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.chatnotificationsmulti;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ChatNotificationsMultiPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ChatNotificationsMultiPlugin.class);
 		RuneLite.main(args);
 	}
 }
